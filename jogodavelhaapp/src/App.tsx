3a44e9e491a5eabbf53a9c5632d6bb771dcdbdff
Ana Oliveira <ana.oliveira@l2code.com.brm.br>
2021-05-19 17:47:18 -0400

    atualizando interface

--- jogodavelhaapp/src/App.tsx
+++ jogodavelhaapp/src/App.tsx
@@ -5,45 +5,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="content">
-            <div className="turno">
-                <h2>Turno</h2>
-                <p>Jogando agora: Jogador</p>
-            </div>
-            <div className="tabuleiro">
-                <div className="linha um">
-                    <div className="coluna um"></div>
-                    <div className="coluna dois"></div>
-                    <div className="coluna tres"></div>
-                </div>
-                <div className="linha dois">
-                    <div className="coluna um"></div>
-                    <div className="coluna dois"></div>
-                    <div className="coluna tres"></div>
-                </div>
-                <div className="linha tres">
-                    <div className="coluna um"></div>
-                    <div className="coluna dois"></div>
-                    <div className="coluna tres"></div>
-                </div>
-            </div>
+      <div className="header">
+        <h1>JOGO DA VELHA</h1>
+        <h3>Desafio L2: React e TS</h3>
+      </div>
+      <div className="main">
+        <div className="content">
+          <div className="turno">
+              <h2>Turno</h2>
+              <p>Jogando agora: Jogador</p>
+          </div>
+          <div className="tabuleiro">
+                  <div className="linha um">
+                      <div className="coluna um"></div>
+                      <div className="coluna dois"></div>
+                      <div className="coluna tres"></div>
+                  </div>
+                  <div className="linha dois">
+                      <div className="coluna um"></div>
+                      <div className="coluna dois"></div>
+                      <div className="coluna tres"></div>
+                  </div>
+                  <div className="linha tres">
+                      <div className="coluna um"></div>
+                      <div className="coluna dois"></div>
+                      <div className="coluna tres"></div>
+                  </div>
+              </div>
         </div>
         <div className="sidebar">
-            <div className="placar">
-                <h2>PLACAR</h2>
-                <p>Partidas jogadas: 0</p>
-                <p>Partida atual: 0</p>
+          <div className="placar">
+                  <h2>PLACAR</h2>
+                  <p>Partidas jogadas: 0</p>
+                  <p>Partida atual: 0</p>
 
-                <h3>Vitórias</h3>
-                <p>Jogador: 0</p>
-                <p>Computador: 0</p>
+                  <h3>Vitórias</h3>
+                  <p>Jogador: 0</p>
+                  <p>Computador: 0</p>
 
-                 <div className="controles">
-                    <button>Reiniciar partida</button>
-                    <button>Riniciar placar</button>
-                </div>
-            </div>
+                  <div className="controles">
+                      <button>Reiniciar partida</button>
+                      <button>Riniciar placar</button>
+                  </div>
+              </div>
         </div>
+      </div>
     </div>
   );
 }
